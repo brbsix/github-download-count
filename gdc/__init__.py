6848cab4e6c21c3e06eeeadf38759d861ccc8d15
Six <brbsix@gmail.com>
2016-04-27 05:08:09 -0700

    Bump version to 0.0.1 for initial release

--- gdc/__init__.py
+++ gdc/__init__.py
@@ -2,5 +2,5 @@
 """Display download counts of GitHub releases."""
 
 __program__ = 'github-download-count'
-__version__ = '0.0.1.dev0'
+__version__ = '0.0.1'
 __description__ = 'Display download counts of GitHub releases'
